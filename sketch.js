139aae7f6e9864b9f073421e9827cd342873cbbf
KartikVarma123 <skv1973@yahoo.com>
2021-04-28 15:54:56 +0530

    kartik

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -34,7 +35,7 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(150,150);
 
 }
 
